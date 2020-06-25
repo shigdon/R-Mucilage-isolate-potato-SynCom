27701d620232b4020722cf6afbf036a8e99317dc
shigdon <smhigdon@ucdavis.edu>
2020-06-25 14:28:48 -0700

    add files to repo

---
+++ Plots.R
@@ -0,0 +1,30 @@
+grid.draw(rbind(ggplotGrob(FW_N1_shoots_bar), ggplotGrob(FW_N1_roots_bar), ggplotGrob(FW_N1_tubers_bar), size = "last"))
+FW_N1_roots_bar
+N1_plots
+N2_plots
+P1_plots
+P2_plots
+
+# All 3 Grid both genotypes
+FW_N1_G1_and_G2_noR1
+FW_N2_G1_and_G2_noR1
+N1_G1_and_G2_tub_num_bar
+FW_N1_G1_and_G2
+FW_N2_G1_and_G2
+
+# Individual Genotype Barstacks
+FW_N1_G1_barstack
+FW_N1_G2_barstack
+FW_N2_G1_barstack
+FW_N2_G2_barstack
+
+FW_N1_G1_barstack_noR1
+FW_N1_G2_barstack_noR1
+FW_N2_G1_barstack_noR1
+FW_N2_G2_barstack_noR1
+
+# Tuber Number
+
+nitrogen_hi_lo_n_tub_all
+N1_G1_and_G2_tub_num_bar
+N2_G1_and_G2_tub_num_bar
